61f537414b2881587df1b74212f0ae3bb8979420
ncgoodbody <nicholas.goodbody@gmail.com>
2021-10-19 15:21:53 -0700

    Update remove_last_blanks.py

--- pre_commit_hooks/remove_last_blanks.py
+++ pre_commit_hooks/remove_last_blanks.py
@@ -1,27 +1,30 @@
-
-import nbformat
 from argparse import ArgumentParser
 from typing import Optional, Sequence
 
+import nbformat
+
+
 def remove_empty_last_cells(filename: str) -> int:
     """
-    Reads notebook from filename. If there are blank cells at the end of the notebook, 
-    removes them, overwrites file, and returns 1. Otherwise, returns 0. 
+    Reads notebook from filename. If there are blank cells at the
+    end of the notebook, removes them, overwrites file, and returns 1.
+    Otherwise, returns 0.
     """
     nb = nbformat.read(filename, as_version=4)
 
     cells = nb["cells"]
     start_cells = len(cells)
-    
-    while cells[-1]['source'] == '':
+
+    while cells[-1]["source"] == "":
         cells.pop()
 
     if len(cells) == start_cells:
         return 0
     else:
         with open(filename, "w") as f:
-            nbformat.write(nb, filename)
+            nbformat.write(nb, f)
         return 1
+
 
 def main(argv: Optional[Sequence[str]] = None) -> int:
     parser = ArgumentParser("Removes blanks cells from end of notebook")
