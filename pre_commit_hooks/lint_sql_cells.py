1ae64df7acef4c7200b2d77df4290eee2c013564
ncgoodbody <nicholas.goodbody@gmail.com>
2021-10-19 15:21:46 -0700

    Update lint_sql_cells.py

--- pre_commit_hooks/lint_sql_cells.py
+++ pre_commit_hooks/lint_sql_cells.py
@@ -1,4 +1,3 @@
-import json
 from argparse import ArgumentParser
 from typing import Optional, Sequence
 
@@ -30,9 +29,9 @@ def clean_sql_cells(filename: str) -> int:
         if (cell["cell_type"] == "code") and ("%%sql" in cell["source"]):
             clean_code = clean_sql_string(cell["source"])
             if cell["source"] != clean_code:
-                cell["source"] = clean_sql_string(cell["source"]).replace('\n\n', "\n")
+                cell["source"] = clean_sql_string(cell["source"]).replace("\n\n", "\n")
                 fail_flag = 1
-    if fail_flag == 1:   
+    if fail_flag == 1:
         with open(filename, "w") as f:
             nbformat.write(nb, f)
         return 1
