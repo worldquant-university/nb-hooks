e6492d369accce00261a017b530f02529a06833b
ncgoodbody <nicholas.goodbody@gmail.com>
2021-10-21 15:35:28 -0700

    Create `add_colophon` module
    Module adds missing colophon or alters out-of-date colophon.

---
+++ pre_commit_hooks/add_colophon.py
@@ -0,0 +1,41 @@
+from argparse import ArgumentParser
+from typing import Optional, Sequence
+
+import nbformat
+from thefuzz import fuzz
+
+from pre_commit_hooks.utils import copyright_text
+
+
+def add_colophon(filename: str) -> int:
+    nb = nbformat.read(filename, as_version=4)
+    cells = nb["cells"]
+
+    # Exact match in last cell
+    if cells[-1]["source"] == copyright_text:
+        return 0
+    # Fuzzy match in last cell, replace text
+    elif 75 < fuzz.ratio(cells[-1]["source"], copyright_text) < 100:
+        cells[-1]["source"] = copyright_text
+    # Add Markdown cell with text
+    else:
+        cells.append(nbformat.v4.new_markdown_cell(source=copyright_text))
+
+    with open(filename, "w") as f:
+        nbformat.write(nb, f)
+
+    return 1
+
+
+def main(argv: Optional[Sequence[str]] = None) -> int:
+    parser = ArgumentParser("Lint notebook cells starting with '%%sql'")
+    parser.add_argument("filenames", nargs="*", help="Filenames for format")
+    args = parser.parse_args(argv)
+
+    for filename in args.filenames:
+        add_colophon(filename)
+    return 0
+
+
+if __name__ == "__main__":
+    exit(main())
---
+++ tests/test_add_colophon.py
@@ -0,0 +1,64 @@
+import nbformat
+import pytest
+
+from pre_commit_hooks.add_colophon import add_colophon
+from pre_commit_hooks.utils import copyright_text
+
+
+def create_base_nb_fixture():
+    nb = nbformat.v4.new_notebook()
+    cells = [
+        nbformat.v4.new_markdown_cell(source="Hello!"),
+        nbformat.v4.new_markdown_cell(source=""),
+        nbformat.v4.new_code_cell(source="1+1"),
+        nbformat.v4.new_code_cell(source=""),
+    ]
+    nb["cells"] = cells
+    return nb
+
+
+@pytest.fixture
+def tmpfiles(tmpdir):
+    # Notebook where everything's correct
+    filename = tmpdir.join("correct_colophon.ipynb")
+    with open(filename, "w") as f:
+        correct_nb = create_base_nb_fixture()
+        correct_nb["cells"].append(nbformat.v4.new_markdown_cell(source=copyright_text))
+        nbformat.write(correct_nb, f)
+
+    # Notebook with copyright from different year
+    filename = tmpdir.join("old_colophon.ipynb")
+    with open(filename, "w") as f:
+        old_colophon_nb = create_base_nb_fixture()
+        old_colophon_nb["cells"].append(
+            nbformat.v4.new_markdown_cell(source=copyright_text.replace("2021", "2020"))
+        )
+        nbformat.write(old_colophon_nb, f)
+
+    # Notebook with no colophon
+    filename = tmpdir.join("no_colophon.ipynb")
+    with open(filename, "w") as f:
+        no_colophon_nb = create_base_nb_fixture()
+        nbformat.write(no_colophon_nb, f)
+
+    yield tmpdir
+
+
+def test_add_colophon(tmpfiles):
+    files = ["correct_colophon.ipynb", "old_colophon.ipynb", "no_colophon.ipynb"]
+    for f in files:
+        add_colophon(tmpfiles.join(f))
+
+    # Notebook cells against which three notebooks will be tested
+    compare = create_base_nb_fixture()
+    compare["cells"].append(nbformat.v4.new_markdown_cell(source=copyright_text))
+    compare_source = [c["source"] for c in compare["cells"]]
+
+    nbs = (nbformat.read(tmpfiles.join(f), as_version=4) for f in files)
+    print("COMPARE")
+    print(compare_source)
+    for nb in nbs:
+        nb_source = [c["source"] for c in nb["cells"]]
+        print("OTHER")
+        print(nb_source)
+        assert all([a == b for a, b in zip(compare_source, nb_source)])
