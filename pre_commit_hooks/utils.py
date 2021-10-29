47c9c724d582a268e2e2018f9102df5f2a1a892f
ncgoodbody <nicholas.goodbody@gmail.com>
2021-10-29 14:07:59 -0700

    Reformat colophon text

--- pre_commit_hooks/utils.py
+++ pre_commit_hooks/utils.py
@@ -1,9 +1,7 @@
 from datetime import datetime
 
-copyright_text = f"""
+copyright_text = f"""---
 Copyright © {datetime.now().strftime("%Y")} WorldQuant University. This
 content is licensed solely for personal use. Redistribution or
 publication of this material is strictly prohibited.
-""".replace(
-    "\n", " "
-).strip()
+"""
