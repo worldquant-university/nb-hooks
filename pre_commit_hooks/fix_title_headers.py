f08c250b2d68db80ea57461a8d414764b54534d5
ncgoodbody <nicholas.goodbody@gmail.com>
2021-11-19 15:31:08 -0800

    Write `check_for_title` func

--- pre_commit_hooks/fix_title_headers.py
+++ pre_commit_hooks/fix_title_headers.py
@@ -1,9 +1,39 @@
+import os
 from argparse import ArgumentParser
 from typing import Optional, Sequence
 
+import nbformat
+from nbformat.notebooknode import NotebookNode
 
-def check_for_title(filename):
-    pass
+
+def read_nb(filename: str) -> NotebookNode:
+    nb = nbformat.read(filename, as_version=4)
+    return nb
+
+
+def check_for_title(filename: str) -> int:
+    nb = read_nb(filename)
+    nb_basename = os.path.basename(filename)
+    first_cell = nb["cells"][0]
+
+    # Check that first cell is Markdown
+    if first_cell["cell_type"] != "markdown":
+        print(f"{nb_basename}: Missing title.")
+        return 1
+
+    # Check that first cell starts w/ proper title formatting
+    if first_cell["source"][:24] != '<font size="+3"><strong>':
+        print(
+            f'{nb_basename}: Missing title formatting tags (`<font size="+3"><strong>`).'
+        )
+        return 1
+
+    # Check that first cell only contains title, check by tags
+    if first_cell["source"][-16:] != "</strong></font>":
+        print(f"{nb_basename}: Additional text in title cell.")
+        return 1
+
+    return 0
 
 
 def main(argv: Optional[Sequence[str]] = None) -> int:
@@ -11,9 +41,11 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
     parser.add_argument("filenames", nargs="*", help="Filenames for format")
     args = parser.parse_args(argv)
 
+    fail_flag = 0
     for filename in args.filenames:
-        check_for_title(filename)
-    return 0
+        if check_for_title(filename) == 1:
+            fail_flag = 1
+    return fail_flag
 
 
 if __name__ == "__main__":
