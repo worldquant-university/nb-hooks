053403e3a846550088373435fb2ec5e689b97dd9
ncgoodbody <nicholas.goodbody@gmail.com>
2022-11-11 12:32:27 -0800

    Remove non utf8 character

--- pre_commit_hooks/utils.py
+++ pre_commit_hooks/utils.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 copyright_text = f"""---
-Copyright © {datetime.now().strftime("%Y")} WorldQuant University. This
+Copyright {datetime.now().strftime("%Y")} WorldQuant University. This
 content is licensed solely for personal use. Redistribution or
 publication of this material is strictly prohibited.
 """
